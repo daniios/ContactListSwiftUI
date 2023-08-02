0c3bef46a129490b4b46bbadf0b9542f2bc2ca7a
Daniil Chupin <danielchupin@yandex.ru>
2023-08-02 16:32:58 +0700

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Даниил Чупин on 02.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
